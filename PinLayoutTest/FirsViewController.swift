79e2473c587e5a5da985bc61a35e68399d1f0eab
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2022-04-27 11:27:22 +0300

    configured SceneDelegate

---
+++ PinLayoutTest/FirsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirsViewController.swift
+//  PinLayoutTest
+//
+//  Created by Евгений Ганусенко on 4/27/22.
+//
+
+import UIKit
+
+class FirsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+    }
+}
